40b1b9da10a800c6579d2c700066b3b8f8928619
linar <lizamakarovna20@gmail.com>
2026-05-13 13:59:18 +0500

    Добавил файлы

--- project.py
+++ project.py
@@ -1,16 +1,4 @@
-with open("file_1", "w") as f:
-    f.write(
-        "\nПривет\nHI, error\nсалам\nerroR, хуфыв, \n12312441asderierrrod\neRrOr_123"
-    )
+import os
 
-with open("file_1", "r") as f:
-    text = f.read()
-
-
-with open("file_1", "w") as f:
-    f.write(text.lower())
-
-with open("file_1", "r") as f:
-    for line in f:
-        if "error" in line:
-            print(line.strip())
+file_name = r"\\wsl.localhost\Ubuntu\home\linar\first_project\songs.txt"
+os.rename(file_name())
---
+++ ЛР43 Тимирханов ИС252.py
@@ -0,0 +1,5 @@
+number = 0
+while number < 15:
+    if number % 2 == 0:
+        number += 1
+        print(number)
